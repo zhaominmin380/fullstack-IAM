967dc8d206743592feadc42e90d12705cdbcc894
zhaominmin380 <bais18624@gmail.com>
2026-07-23 01:45:26 +0800

    update JWT

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -5,7 +5,7 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'dev-4xkuepyf61nqzkrs.us.auth0.com'
+AUTH0_DOMAIN = 'dev-wcqvzlc8letz1j52.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'Image'
 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-4xkuepyf61nqzkrs.us', // the auth0 domain prefix
+    url: 'dev-wcqvzlc8letz1j52.us', // the auth0 domain prefix
     audience: 'Image', // the audience set for the auth0 app
-    clientId: '6063unVMnEqeNQdY6GK4yROrWfckuJhK', // the client id generated for the auth0 app
+    clientId: 'XkZo2R11uPQweRe9dCruFygYLwu09yjA', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
